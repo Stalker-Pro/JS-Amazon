5ba9b0f47ca2c0d1e4cc5a3471c676773c89b218
Stalker-Pro <andreit007@mail.ru>
2024-12-01 20:47:33 +0700

    good

--- JS code/11-todo-list.js
+++ JS code/11-todo-list.js
@@ -1,12 +1,41 @@
-const todoList = [];
 const inputTodo = document.querySelector('.js-name-input');
+const inputText = document.querySelector('.todo-list');
 
+// let todoList = ['create code', 'Watch movie'];
+let todoList = JSON.parse(localStorage.getItem('todoList')) || [];
+
+
+
+renderTodoList();
+
+
+function renderTodoList() {
+  let todoListHtml = '';
+
+  for (let i = 0; i < todoList.length; i++) {
+    const html = `<p> ${todoList[i]}</p>`;
+    todoListHtml += html;
+  }
+
+  console.log(todoListHtml);
+  inputText.innerHTML = todoListHtml;
+}
+
+function clearTodoList() {
+  inputText.innerHTML = '';
+  todoList = [];
+  localStorage.removeItem('todoList');
+}
 
 function addTodo(){
   let val = inputTodo.value;
   todoList.push(val);
   console.log(todoList);
   inputTodo.value = '';
+
+  localStorage.setItem('todoList',JSON.stringify(todoList));
+
+  renderTodoList();
 }
 
 function keyEnter(e){
